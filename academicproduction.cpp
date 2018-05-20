6debaebfe0ffcfcf624cba806020d4a81b65504f
AlexAlvrzCastell <39219220+AlexAlvrzCastell@users.noreply.github.com>
2018-05-20 10:01:34 -0500

    Actualización de clase
    Se cambia el atributo nombre (tipo Name) a (tipo String)

--- academicproduction.cpp
+++ academicproduction.cpp
@@ -1,79 +1,72 @@
 #include "academicproduction.h"
 ///Constructor Base
-AcademicProduction::AcademicProduction()
-{
-        type = "NON";
-        id = 0;
-        status = "NON";
-}
+AcademicProduction::AcademicProduction() {
+    name = "NON";
+    type = "NON";
+    id = "NON";
+    status = "NON";
+    }
 
 ///Constructor Copia
-AcademicProduction::AcademicProduction(const AcademicProduction& e) : name(e.name), type(e.type), author(e.author), id(e.id), status(e.status){ }
+AcademicProduction::AcademicProduction(const AcademicProduction& e) : name(e.name), type(e.type), author(e.author), id(e.id), status(e.status) { }
 
 ///Constructor Parametrizado
-AcademicProduction::AcademicProduction(const Name& n, std::string& t, Date& d, Name& a, int& i, std::string& s) : name(n), type(t), elaborationDate(d), author(a), id(i), status(s){ }
+AcademicProduction::AcademicProduction(const std::string& n, std::string& t, Date& d, Name& a, std::string& i, std::string& s) : name(n), type(t), elaborationDate(d), author(a), id(i), status(s) { }
 
 
-Name AcademicProduction::getName()
-{
+std::string AcademicProduction::getName() {
     return name;
-}
+    }
 
-std::string AcademicProduction::getType()
-{
+std::string AcademicProduction::getType() {
     return type;
-}
+    }
 
-Date AcademicProduction::getElaborationDate()
-{
+Date AcademicProduction::getElaborationDate() {
     return elaborationDate;
-}
+    }
 
-int AcademicProduction::getId()
-{
+std::string AcademicProduction::getId() {
     return id;
-}
+    }
 
-std::string AcademicProduction::getStatus()
-{
+std::string AcademicProduction::getStatus() {
     return status;
+    }
+
+Name AcademicProduction::getAuthor(){
+    return author;
 }
 
-void AcademicProduction::setName(const Name& n)
-{
+void AcademicProduction::setName(const std::string& n) {
     name = n;
-}
+    }
 
-void AcademicProduction::setType(const std::string& t)
-{
+void AcademicProduction::setType(const std::string& t) {
     type = t;
-}
+    }
 
-void AcademicProduction::setElaborationDate(const Date& d)
-{
+void AcademicProduction::setElaborationDate(const Date& d) {
     elaborationDate = d;
-}
+    }
 
-void AcademicProduction::setAuthor(const Name& a)
-{
+void AcademicProduction::setAuthor(const Name& a) {
     author = a;
-}
+    }
 
-void AcademicProduction::setId(const int& i)
-{
+void AcademicProduction::setId(const std::string& i) {
     id = i;
-}
+    }
 
-void AcademicProduction::setStatus(const std::string& s)
-{
+void AcademicProduction::setStatus(const std::string& s) {
     status = s;
-}
+    }
 
-std::string AcademicProduction::toString(){
+std::string AcademicProduction::toString() {
     std::string result;
 
     result = "Nombre: ";
-    result += name.toString();
+    result += name;
     result += "Tipo: ";
     result += type;
     result += "Fecha De Elaboracion: ";
@@ -86,4 +79,4 @@ std::string AcademicProduction::toString(){
     result += status;
 
     return result;
-}
+    }
